1c306edd2a262acdd47d645f402173911c1d8245
domkh@12 <dommr86@gmail.com>
2026-02-15 10:43:22 +0700

    first commit

---
+++ src/main/java/site/secmega/secapi/SecApiApplication.java
@@ -0,0 +1,14 @@
+package site.secmega.secapi;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/site/secmega/secapi/SecApiApplicationTests.java
@@ -0,0 +1,13 @@
+package site.secmega.secapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
